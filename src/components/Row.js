301fcde990580949e857ff6ed4fbd2b9113be09f
Mark Gable <mgable43@gmail.com>
2018-03-24 15:05:55 -0700

    make whole row clickable

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,7 +1,7 @@
 import React  from 'react';
 
 function Row(props) {
- 	return <tr className="table-row" id={'tr-' + props.idx}><td><div onClick={() => {props.onClick(props.item, props.idx)}} className="name left-align">{props.item.name}</div></td><td className="row-holder"><div className="row-background"><div className="total" style={props.style}>&nbsp;</div></div></td><td>{props.item.count}</td></tr>
+ 	return <tr className="table-row" id={'tr-' + props.idx} onClick={() => {props.onClick(props.item, props.idx)}} ><td><div className="name left-align">{props.item.name}</div></td><td className="row-holder"><div className="row-background"><div className="total" style={props.style}>&nbsp;</div></div></td><td>{props.item.count}</td></tr>
 }
 
 export default Row
